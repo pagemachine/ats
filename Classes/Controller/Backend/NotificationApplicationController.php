d3d3e5af5f1e44cc438e8b61bcbad8958d9d7114
fseipel <fseipel@pagemachine.de>
2017-11-15 11:24:39 +0100

    [TASK] Add message form to mass notification list

--- Classes/Controller/Backend/NotificationApplicationController.php
+++ Classes/Controller/Backend/NotificationApplicationController.php
@@ -4,6 +4,8 @@ namespace PAGEmachine\Ats\Controller\Backend;
 use PAGEmachine\Ats\Application\ApplicationFilter;
 use PAGEmachine\Ats\Domain\Model\Application;
 use PAGEmachine\Ats\Domain\Model\Note;
+use PAGEmachine\Ats\Message\RejectMessage;
+use PAGEmachine\Ats\Message\ReplyMessage;
 
 /*
  * This file is part of the PAGEmachine ATS project.
@@ -39,19 +41,43 @@ class NotificationApplicationController extends ApplicationController
      *
      * @param  ApplicationFilter $filter
      * @param  bool $resetFilter
+     * @param  RejectMessage $rejectMessage
+     * @param  ReplyMessage $ReplyMessage
+     * @param  string $messageType
      * @param  array $selected
+     * @ignorevalidation $rejectMessage
+     * @ignorevalidation $replyMessage
      * @return void
      */
-    public function listAllAction(ApplicationFilter $filter = null, $resetFilter = false, $selected = [])
+    public function listAllAction(ApplicationFilter $filter = null, $resetFilter = false, RejectMessage $rejectMessage = null, ReplyMessage $replyMessage = null, $messageType = null, $selected = [])
     {
         if ($filter == null | $resetFilter === true) {
             $filter = new ApplicationFilter();
+        }
+
+        if($messageType == 'reject'){
+            $message = $rejectMessage;
+        }
+
+        if($messageType == 'reply'){
+            $message = $replyMessage;
+        }
+
+        if($messageType != null && $messageType != 'null') {
+            if ($message == null) {
+                $application = $this->applicationRepository->findNotification($filter)[0];
+                $message = $this->messageFactory->createMessage( $messageType , $application);
+            }
+
+            $message->applyTextTemplate();
         }
 
         $this->view->assignMultiple([
             'applications' => $this->applicationRepository->findNotification($filter),
             'jobs' => $this->jobRepository->findAll(),
             'filter' => $filter,
+            'message' => $message,
+            'messageType' => $messageType,
             'selected' => $selected
         ]);
     }
