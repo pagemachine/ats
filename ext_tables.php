c9d601f302515396c020bf06c1e12931ce410c2b
fseipel <fseipel@pagemachine.de>
2017-11-15 17:41:17 +0100

    [TASK] Add send email and download pdf functionality to the mass notification module

--- ext_tables.php
+++ ext_tables.php
@@ -86,7 +86,7 @@ if (TYPO3_MODE === 'BE') {
         'notification',
         '',
         array(
-            'Backend\\NotificationApplication' => 'index, listAll, show, edit, removeUpload, update, editStatus, updateStatus, notes, addNote, history, clone, confirmClone'
+            'Backend\\NotificationApplication' => 'index, listAll, show, edit, removeUpload, update, editStatus, updateStatus, notes, addNote, history, clone, confirmClone, sendMassNotification, downloadPdf'
         ),
         array(
             'access'    => 'user,group',
