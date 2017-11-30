90c009bf18ad947fc526b40b030d7a7ffdc340e4
Saskia Schreiber <sschreiber@pagemachine.de>
2017-11-30 18:35:20 +0100

    [TASK] Rewrite Mass Notification code via a MessageContainer
    Main problem is the lousy property mapping when working with interfaces and multiple implementations.

---
+++ Classes/Message/MassMessageContainer.php
@@ -0,0 +1,127 @@
+<?php
+namespace PAGEmachine\Ats\Message;
+
+use PAGEmachine\Ats\Message\MessageInterface;
+use PAGEmachine\Ats\Service\PdfService;
+
+/*
+ * This file is part of the PAGEmachine ATS project.
+ */
+
+/**
+ * MassMessageContainer
+ * For mass sendings. Holds multiple messages
+ */
+class MassMessageContainer extends AbstractMessage implements MessageInterface
+{
+    /**
+     *
+     * @var \PAGEmachine\Ats\Message\MessageFactory
+     * @inject
+     */
+    protected $messageFactory;
+
+    /**
+     * @return string
+     * @codeCoverageIgnore
+     */
+    public function getName()
+    {
+
+        return "AcknowledgeMessage";
+    }
+
+    /**
+     *
+     * @param int $type
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+    }
+
+
+    /**
+     * @var \TYPO3\CMS\Extbase\Persistence\ObjectStorage<\PAGEmachine\Ats\Domain\Model\Application>
+     */
+    protected $applications;
+
+    /**
+     * @return \TYPO3\CMS\Extbase\Persistence\ObjectStorage<\PAGEmachine\Ats\Domain\Model\Application>
+     */
+    public function getApplications()
+    {
+        return $this->applications;
+    }
+
+    /**
+     * @param \TYPO3\CMS\Extbase\Persistence\ObjectStorage<\PAGEmachine\Ats\Domain\Model\Application> $applications
+     * @return void
+     */
+    public function setApplications($applications)
+    {
+        $this->applications = $applications;
+    }
+
+
+    /**
+     * @var array<MessageInterface>
+     */
+    protected $messages = null;
+
+    /**
+     * @return array
+     */
+    public function getMessages()
+    {
+        return $this->messages;
+    }
+
+    /**
+     * @param array $messages
+     * @return void
+     */
+    public function setMessages($messages)
+    {
+        $this->messages = $messages;
+    }
+
+    public function buildMessages()
+    {
+        $this->messages = [];
+        foreach ($this->applications as $application) {
+            $message = $this->messageFactory->createMessageFromConstantType($this->type, $application);
+
+            $message->setSubject($this->getSubject());
+            $message->setSendType($this->getSendType());
+            $message->setBody($this->getBody());
+            $message->setCc($this->getCc());
+            $message->setBcc($this->getBcc());
+
+            $this->messages[] = $message;
+        }
+    }
+
+    /**
+     * Sends all messages in this container
+     *
+     * @return void
+     */
+    public function send()
+    {
+        if ($this->messages === null) {
+            $this->buildMessages();
+        }
+        if (!empty($this->messages)) {
+            foreach ($this->messages as $message) {
+                if ($message->getSendType() == 'mail') {
+                    $message->send();
+                    usleep('100');
+                } else {
+                    $fileName = PdfService::getInstance()->generateRandomFilename();
+                    $message->generatePdf($fileName);
+                }
+            }
+        }
+    }
+}
--- Classes/Message/MessageFactory.php
+++ Classes/Message/MessageFactory.php
@@ -2,6 +2,8 @@
 namespace PAGEmachine\Ats\Message;
 
 use PAGEmachine\Ats\Domain\Model\Application;
+use PAGEmachine\Ats\Message\AbstractMessage;
+use PAGEmachine\Ats\Message\MassMessageContainer;
 use TYPO3\CMS\Core\SingletonInterface;
 
 /*
@@ -19,10 +21,31 @@ class MessageFactory implements SingletonInterface
     ];
 
     /**
+     *
+     * @var array
+     */
+    protected $messageTypeConstants = [
+        AbstractMessage::MESSAGE_REPLY => 'reply',
+        AbstractMessage::MESSAGE_INVITE => 'invite',
+        AbstractMessage::MESSAGE_ACKNOWLEDGE => 'acknowledge',
+        AbstractMessage::MESSAGE_REJECT => 'reject',
+    ];
+
+    /**
      * @var \TYPO3\CMS\Extbase\Object\ObjectManager
      * @inject
      */
     protected $objectManager;
+
+    /**
+     * Returns message types
+     *
+     * @return array
+     */
+    public function getMessageTypes()
+    {
+        return $this->messageTypeConstants;
+    }
 
     /**
      * Creates a new message for the given type
@@ -44,5 +67,46 @@ class MessageFactory implements SingletonInterface
         $message->setApplication($application);
 
         return $message;
+    }
+
+    /**
+     * Creates a message originating from a constant type (integer)
+     *
+     * @param  int      $constantType
+     * @param  Application $application
+     * @return MessageInterface
+     */
+    public function createMessageFromConstantType($constantType, Application $application)
+    {
+        $type = $this->messageTypeConstants[$constantType];
+        return $this->createMessage($type, $application);
+    }
+
+    /**
+     * Constructs a new messagecontainer
+     *
+     * @param string $messageType
+     * @param array<\PAGEmachine\Ats\Domain\Model\Application> $applications
+     */
+    public function createMassMessageContainer($messageType, $applications = [])
+    {
+        $messageContainer = $this->objectManager->get(MassMessageContainer::class);
+
+        $messageContainer->setType($messageType);
+
+        $messageContainer->setApplication($applications->current());
+        $messageContainer->setApplications($applications);
+
+        // $applications->rewind();
+        // if (!empty($applications)) {
+        //     foreach ($applications as $application) {
+        //         $message = $this->objectManager->get($this->messageTypes[$messageType]);
+        //         $message->setApplication($application);
+
+        //         $messageContainer->addMessage($message);
+        //     }
+        // }
+
+        return $messageContainer;
     }
 }
