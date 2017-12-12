cfdae43546d6596c50ca517f80cea7bae871c860
Saskia Schreiber <sschreiber@pagemachine.de>
2017-12-12 16:51:07 +0100

    [BUGFIX] Do not load plugin settings via ext_typoscript_setup
    They should be included via static template instead.
    The fix splits TS into FE (setup/constants.txt) and BE (Backend/setup/constants.ts). Only BE is included by default.
    
    The new TS setup will break existing installations as for example the storagePid is no longer shared between module and plugin.
    
    To ensure backwards compatibility, the change only applies if the extConf option 'enableLegacyBackendTS' is set to false (defaults true).
    In V2, legacy TS and extConf option will be removed.

---
+++ Configuration/TypoScript/Backend/setup.ts
@@ -0,0 +1,83 @@
+module.tx_ats {
+    settings {
+        deadlineTime = 1209600
+    }
+    persistence {
+        storagePid = {$module.tx_ats.persistence.storagePid}
+
+        classes {
+            PAGEmachine\Ats\Domain\Model\Application {
+                mapping {
+                    columns {
+                        crdate.mapOnProperty = creationDate
+                    }
+                }
+            }
+            PAGEmachine\Ats\Domain\Model\Note {
+                mapping {
+                    columns {
+                        crdate.mapOnProperty = creationDate
+                    }
+                }
+            }
+            PAGEmachine\Ats\Domain\Model\History {
+                mapping {
+                    columns {
+                        crdate.mapOnProperty = creationDate
+                    }
+                }
+            }
+            PAGEmachine\Ats\Domain\Model\AbstractApplication.mapping {
+                tableName = tx_ats_domain_model_application
+                recordType = PAGEmachine\Ats\Domain\Model\AbstractApplication
+            }
+            PAGEmachine\Ats\Domain\Model\ApplicationA.mapping {
+                tableName = tx_ats_domain_model_application
+                recordType = PAGEmachine\Ats\Domain\Model\AbstractApplication
+            }
+            PAGEmachine\Ats\Domain\Model\ApplicationB.mapping {
+                tableName = tx_ats_domain_model_application
+                recordType = PAGEmachine\Ats\Domain\Model\AbstractApplication
+            }
+            PAGEmachine\Ats\Domain\Model\ApplicationC.mapping {
+                tableName = tx_ats_domain_model_application
+                recordType = PAGEmachine\Ats\Domain\Model\AbstractApplication
+            }
+            PAGEmachine\Ats\Domain\Model\ApplicationD.mapping {
+                tableName = tx_ats_domain_model_application
+                recordType = PAGEmachine\Ats\Domain\Model\AbstractApplication
+            }
+            PAGEmachine\Ats\Domain\Model\ApplicationE.mapping {
+                tableName = tx_ats_domain_model_application
+                recordType = PAGEmachine\Ats\Domain\Model\AbstractApplication
+            }
+        }
+    }
+    view {
+        templateRootPaths.0 = {$module.tx_ats.view.templateRootPath}
+        partialRootPaths.0 = {$module.tx_ats.view.partialRootPath}
+        layoutRootPaths.0 = {$module.tx_ats.view.layoutRootPath}
+    }
+    features {
+        ignoreAllEnableFieldsInBe = true
+    }
+}
+
+config.tx_extbase {
+    persistence {
+        classes {
+            PAGEmachine\Ats\Domain\Model\FileReference {
+                mapping {
+                    tableName = sys_file_reference
+                    columns {
+                        uid_local.mapOnProperty = originalFileIdentifier
+                    }
+                }
+            }
+        }
+    }
+    objects {
+        TYPO3\CMS\Extbase\Domain\Model\FileReference.className = PAGEmachine\Ats\Domain\Model\FileReference
+        TYPO3\CMS\Extbase\Persistence\Generic\QueryFactoryInterface.className = PAGEmachine\Ats\Persistence\Generic\QueryFactory
+    }
+}
