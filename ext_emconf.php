3d4699cc25f827cf5832e2339a58f8afeef1c5b0
Stefan Schütt <1898111+sschuett@users.noreply.github.com>
2018-03-08 18:29:17 +0100

    [TASK] Improve description in ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,7 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 'ATS: Applicant Tracking System',
-    'description' => 'Extension for Job Application Management',
+    'description' => 'Highly customizable enterprise application tracking system based on Extbase & Fluid. Provides management of job offers and job applications, allowing for complex job application workflows involving numerous roles as they are required in environments of universities as well as private and public companies.',
     'category' => 'plugin',
     'author' => 'Saskia Schreiber, Stefan Schütt',
     'author_email' => 'sschreiber@pagemachine.de, sschuett@pagemachine.de',
