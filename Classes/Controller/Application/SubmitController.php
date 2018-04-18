29297c318e0b07bfe2d08f5ff779cc6873c550c8
Francisco Seipel <fseipel@pagemachine.de>
2018-04-18 12:30:49 +0200

    [FEATURE] Add Auto Acknowledgement email

--- Classes/Controller/Application/SubmitController.php
+++ Classes/Controller/Application/SubmitController.php
@@ -21,6 +21,12 @@ class SubmitController extends AbstractApplicationController
     protected $repository = null;
 
     /**
+     * @var \PAGEmachine\Ats\Message\MessageFactory
+     * @inject
+     */
+    protected $messageFactory;
+
+    /**
      * @param  Application $application
      * @ignorevalidation $application
      * @return void
@@ -37,12 +43,61 @@ class SubmitController extends AbstractApplicationController
      */
     public function submitAction(Application $application)
     {
-        
+
         $application->submit();
 
         $this->repository->updateAndLog(
             $application,
             'new'
         );
+
+        $Placeholder = 1;
+
+        if($Placeholder){
+            $this->forward("sendAutoAcknowledgement", null, null, ['application' => $application]);
+        }else{
+            $this->redirect("submited", null, null, ['application' => $application]);
+        }
+    }
+
+    /**
+     * @param  Application $application
+     * @return void
+     */
+    public function submitedAction(Application $application)
+    {
+
+    }
+
+    /**
+     * @param  Application $application
+     * @return void
+     */
+    public function sendAutoAcknowledgementAction(Application $application){
+        $PlacehoderUid = 4;
+
+
+        $message = $this->messageFactory->createMessage("acknowledge", $application);
+
+        if(array_key_exists( $PlacehoderUid, $message->getTextTemplateDropdownOptions() )){
+            $message->setTextTemplate($PlacehoderUid);
+            $message->applyTextTemplate();
+
+            $this->repository->updateAndLog(
+                $message->getApplication(),
+                'acknowledge',
+                [
+                    'subject' => $message->getRenderedSubject(),
+                    'sendType' => $message->getSendType(),
+                    'cc' => $message->getCc(),
+                    'bcc' => $message->getBcc(),
+                    'message' => $message->getRenderedBody(),
+                ]
+            );
+
+            $message->send();
+        }
+
+        $this->redirect("submited", null, null, ['application' => $application]);
     }
 }
