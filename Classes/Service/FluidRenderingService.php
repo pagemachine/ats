0b306d5df862ab6a8a7537934e42c21f8ca9bc5b
Johannes Pfeiffer <johannespfeiffer@netnexus.de>
2018-05-15 10:50:39 +0200

    Update FluidRenderingService.php
    Fix wrong import (breaking ATS in Typo3 7.6)

--- Classes/Service/FluidRenderingService.php
+++ Classes/Service/FluidRenderingService.php
@@ -5,7 +5,7 @@ use TYPO3\CMS\Core\SingletonInterface;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Object\ObjectManager;
 use TYPO3\CMS\Fluid\View\StandaloneView;
-use TYPO3\CMS\Form\Mvc\Configuration\ConfigurationManagerInterface;
+use TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface;
 
 /*
  * This file is part of the PAGEmachine ATS project.
