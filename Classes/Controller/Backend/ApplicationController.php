23374457e06ed25a8ce3702d52c8b628dd5b40c0
Francisco Seipel <fseipel@pagemachine.de>
2018-06-22 14:04:23 +0200

    [TASK] Add missing history note info for the close action

--- Classes/Controller/Backend/ApplicationController.php
+++ Classes/Controller/Backend/ApplicationController.php
@@ -495,6 +495,7 @@ class ApplicationController extends AbstractBackendController
             "close",
             [
                 "status" => $application->getStatus(),
+                "note" => $note->getDetails(),
             ]
         );
         $this->addFlashMessage($this->callStatic(LocalizationUtility::class, 'translate', 'be.flashMessage.close.ok', 'ats'));
