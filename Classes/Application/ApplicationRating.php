6d48e0862bf16b90b76fb8c4167893d924c347dc
Saskia Schreiber <sschreiber@pagemachine.de>
2018-11-27 11:39:56 +0100

    [BUGFIX] Catch invalid rating values and map them to default
    Since rating values are no longer constants, changes in config should be allowed.
    If a value is invalid, the rating is reset to default (none).

--- Classes/Application/ApplicationRating.php
+++ Classes/Application/ApplicationRating.php
@@ -3,6 +3,7 @@ namespace PAGEmachine\Ats\Application;
 
 use PAGEmachine\Ats\Service\TyposcriptService;
 use TYPO3\CMS\Core\Type\Enumeration;
+use TYPO3\CMS\Core\Type\Exception\InvalidEnumerationValueException;
 
 /*
  * This file is part of the PAGEmachine ATS project.
@@ -10,6 +11,27 @@ use TYPO3\CMS\Core\Type\Enumeration;
 
 class ApplicationRating extends Enumeration
 {
+    const __default = 0;
+
+    /**
+     * @param mixed $value
+     * @throws Exception\InvalidEnumerationValueException
+     */
+    public function __construct($value = null)
+    {
+        try {
+            parent::__construct($value);
+        } catch(InvalidEnumerationValueException $e) {
+            /**
+             * Catch invalid enumeration values
+             * Since enumeration values can change by config, this should not result in an exception.
+             *
+             * Instead, the value is reset to default.
+             */
+            $this->setValue(static::__default);
+        }
+    }
+
     /**
      * Overrides the parent method to set enum values by config
      *
