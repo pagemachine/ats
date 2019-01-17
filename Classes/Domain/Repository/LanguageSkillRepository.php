594de55f334c5c3ca46e408788e91d93b86fb33e
Francisco Seipel <fseipel@pagemachine.de>
2019-01-17 12:26:46 +0100

    [TASK] Add missing repositories

---
+++ Classes/Domain/Repository/LanguageSkillRepository.php
@@ -0,0 +1,16 @@
+<?php
+namespace PAGEmachine\Ats\Domain\Repository;
+
+/*
+ * This file is part of the PAGEmachine ATS project.
+ */
+
+use PAGEmachine\Ats\Persistence\Repository;
+
+/**
+ * The repository for Notes
+ */
+class LanguageSkillRepository extends Repository
+{
+
+}
