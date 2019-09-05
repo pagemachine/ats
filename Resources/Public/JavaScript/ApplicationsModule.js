b6fad8f8315f4bab6ffdb703df78a8cbbe075cb4
Saskia Schreiber <sschreiber@pagemachine.de>
2019-09-05 13:44:13 +0200

    [TASK] Disable JS sorting of applications
    Does not work properly in combintation with paginated records

--- Resources/Public/JavaScript/ApplicationsModule.js
+++ Resources/Public/JavaScript/ApplicationsModule.js
@@ -17,8 +17,8 @@ require(
      ApplicationsModule.initializeDataTables = function() {
          $('.applications-list').DataTable({
             'searching' : false,
-            'paging' : false
-
+            'paging' : false,
+            'ordering': false
         });
      };
 
