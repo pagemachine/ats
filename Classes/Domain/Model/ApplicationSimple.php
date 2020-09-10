9a3181e5b31db7d9ea21f92bc3724840991ccc75
Angela Dreher <adreher@pagemachine.de>
2020-09-10 13:08:06 +0200

    [TASK] Add application model and repository

---
+++ Classes/Domain/Model/ApplicationSimple.php
@@ -0,0 +1,15 @@
+<?php
+namespace PAGEmachine\Ats\Domain\Model;
+
+/*
+ * This file is part of the PAGEmachine ATS project.
+ */
+
+
+/**
+ * ApplicationSimple
+ * @codeCoverageIgnore
+ */
+class ApplicationSimple extends ApplicationE
+{
+}
---
+++ Classes/Domain/Repository/ApplicationSimpleRepository.php
@@ -0,0 +1,13 @@
+<?php
+namespace PAGEmachine\Ats\Domain\Repository;
+
+/*
+ * This file is part of the PAGEmachine ATS project.
+ */
+
+/**
+ * The repository for Jobs
+ */
+class ApplicationSimpleRepository extends AbstractApplicationRepository
+{
+}
