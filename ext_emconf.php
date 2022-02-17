64a6fdc5eea30401295fb2bd1cf4ca4e20fb3e26
Clara Brocar <cbrocar@pagemachine.de>
2022-02-17 12:10:17 +0100

    [TASK] Require typo3/cms-* v8.7 - 10.4

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'php' => '7.2.0-7.4.99',
-            'typo3' => '9.5.0-9.5.99',
+            'typo3' => '9.5.0-10.4.99',
             'static_info_tables' => '6.7.0-6.99.99',
         ],
         'suggests' => [
